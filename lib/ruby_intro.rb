d156419c076ff67fa0e060868eeceb371e0e1d5a
hundisilm <mait.raidmae@gmail.com>
2015-10-25 23:24:49 +0000

    HW0

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,87 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  sum = 0
+  arr.each{|x| sum+=x}
+  return sum
 end
 
+
 def max_2_sum arr
-  # YOUR CODE HERE
+  sum = 0
+  if arr.length == 0
+    sum = 0
+  elsif (arr.length == 1)
+    sum = arr[0]
+  else
+    sorted = arr.sort!.reverse
+    sum=sorted[0]+sorted[1]
+  end
+  return sum
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  sumTrue = false
+  temp = 0
+  for i in 0..arr.length-1
+    j = i + 1
+    temp = arr[i]
+    for j in i+1..arr.length-1
+      if temp + arr[j] == n
+        sumTrue = true
+      end 
+    end 
+  end 
+  return sumTrue
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  return "Hello, #{name}"
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  if s =~ /^[a-z]/i && !(s =~ /^[aeoui]/i)
+    return true
+  end
+  return false
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  # Check for valid binary
+  if s =~ /^[01]+$/i
+    num = s.to_i(2)
+    if num%4 == 0
+      return true
+    end
+  end
+  return false
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  def initialize(isbn, price)
+    
+  if price<=0 || isbn.length == 0
+    raise ArgumentError
+  end
+    
+    @isbn   = isbn
+    @price  = price
+    
+  end
+  
+  attr_accessor :isbn
+  attr_accessor :price
+  
+  def price_as_string()
+    return "$#{'%.2f' % @price}"
+  end
+  
 end
+
+book = BookInStock.new("s",3)
+
+puts book.price_as_string()
